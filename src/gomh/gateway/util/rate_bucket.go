e3b8828444c2ae359b847491c711ff048a107377
moxiaomomo@gmail.com <moxiaomomo@gmail.com>
2018-05-28 12:09:41 -0400

    update rate limit logic.

--- src/gomh/gateway/util/rate_bucket.go
+++ src/gomh/gateway/util/rate_bucket.go
@@ -6,23 +6,34 @@ import (
 )
 
 type TokenBucket struct {
-	mutex  sync.Mutex
-	ticker *time.Ticker
+	mutex       sync.Mutex
+	ticker      *time.Ticker
+	ticker2     *time.Ticker
+	lastAjustAt time.Time
 
-	interval time.Duration
+	interval  time.Duration
+	interval2 time.Duration
 
 	capacity   int64
 	availcount int64
+	multiple   int64
 }
 
 func NewTokenBucket(cap int64, interval time.Duration) *TokenBucket {
-	return &TokenBucket{
+	tb := &TokenBucket{
 		interval:   interval,
 		capacity:   cap,
 		availcount: cap,
 
-		ticker: time.NewTicker(interval),
+		ticker2: time.NewTicker(100 * time.Millisecond),
 	}
+
+	tb.interval2 = 100 * time.Millisecond
+	tb.multiple = tb.multipleOfIntervalInFact()
+	tb.lastAjustAt = time.Now()
+	go tb.genTokenWithTicker_v2()
+
+	return tb
 }
 
 func (tb *TokenBucket) genTokenWithTicker() {
@@ -35,6 +46,22 @@ func (tb *TokenBucket) genTokenWithTicker() {
 	}
 }
 
+func (tb *TokenBucket) genTokenWithTicker_v2() {
+	for _ = range tb.ticker2.C {
+		tb.mutex.Lock()
+
+		tb.availcount += tb.multiple
+		if tb.availcount > tb.capacity {
+			tb.availcount = tb.capacity
+		}
+		tb.mutex.Unlock()
+	}
+}
+
+func (tb *TokenBucket) adjust() {
+
+}
+
 func (tb *TokenBucket) TryTake(n int64) bool {
 	tb.mutex.Lock()
 	defer tb.mutex.Unlock()
@@ -45,4 +72,12 @@ func (tb *TokenBucket) TryTake(n int64) bool {
 	}
 
 	return false
+}
+
+func (tb *TokenBucket) multipleOfIntervalInFact() int64 {
+	val := tb.interval2.Nanoseconds() / tb.interval.Nanoseconds()
+	if val < 1 {
+		val = 1
+	}
+	return val
 }
